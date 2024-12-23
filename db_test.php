fde8d93e0391b16572789aa090ebe6cfc9031dfa
baig892 <aliyanbaig.303@gmail.com>
2024-12-23 12:46:38 +0500

    update file

--- db_test.php
+++ db_test.php
@@ -2,7 +2,6 @@
 $host = 'databaseone.c7gmk6ywo6rb.us-east-1.rds.amazonaws.com';
 $user = 'admin';
 $password = 'Ox4qfGyFfqBDPVQ8vWGu';
-$dbname = '-';
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
