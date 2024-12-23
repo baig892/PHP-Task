994cd88417c8a49793bbfd2dd5dff4378f85067b
baig892 <aliyanbaig.303@gmail.com>
2024-12-23 10:26:41 +0500

    Updated file

--- inc/connection.php
+++ inc/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['conn'] = mysqli_connect("localhost", "root", "", "hzcotfyp_p1sla");
+$GLOBALS['conn'] = mysqli_connect("localhost", "root", "", "databaseone");
  // $GLOBALS['conn'] = mysqli_connect("premium34.web-hosting.com", "hzcotfyp_p1sla_admin_user", "c2t6tE3tK@zgaBk", "hzcotfyp_p1sla");
 
 //  $GLOBALS['root'] = "/p1sla/";
